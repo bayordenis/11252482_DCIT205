083e3e444c4b58e22591283c466224673264f0f0
DenisBayor <bayordenis766@gmail.com>
2023-11-25 13:05:44 +0000

    Removed file

--- index.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('myIframe').onload = function() {
-    var iframe = document.getElementById('myIframe');
-    var iframeSrc = iframe.src;
-    iframe.src = iframeSrc + '?autoplay=1';
-  };
